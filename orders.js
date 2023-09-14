d2b503a06aaffa9f9debd689e82876368a85f054
La-sie <fsdjogatse@st.ug.ed.gh>
2023-09-14 15:08:12 +0000

    Last Commit

---
+++ orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '#A6572',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'LAVENDER KF!)" Drone',
+        productNumber: '#A9875',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Ruko F11 Pro Drone',
+        productNumber: '#A8764',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Drone with Camera Drone',
+        productNumber: '#A3214',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'GPS 4k Drone',
+        productNumber: '#A7654',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+]
